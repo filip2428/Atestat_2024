93680771e54a1b4abb71d76239547403d857f000
filip2428 <165894116+filip2428@users.noreply.github.com>
2024-04-04 08:09:22 +0300

    test

--- Atestat/Atestat/Adaugare_carte.Designer.cs
+++ Atestat/Atestat/Adaugare_carte.Designer.cs
@@ -42,6 +42,8 @@
             this.label6 = new System.Windows.Forms.Label();
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -164,17 +166,37 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(83, 498);
+            this.label7.Location = new System.Drawing.Point(39, 498);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(224, 29);
+            this.label7.Size = new System.Drawing.Size(268, 29);
             this.label7.TabIndex = 13;
-            this.label7.Text = "Exemplare Deținute";
+            this.label7.Text = "Exemplare Împrumutate";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(354, 575);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(266, 36);
+            this.textBox8.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(221, 575);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(86, 29);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Poziție";
             // 
             // Adaugare_carte
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1224, 668);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.label6);
@@ -212,5 +234,7 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
     }
 }
